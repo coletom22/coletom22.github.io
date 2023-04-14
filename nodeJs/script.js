1c43329d1e0f0c5528b6ee30d320dadad3b1fcff
coletom22 <78000402+coletom22@users.noreply.github.com>
2023-04-13 20:59:15 -0700

    contact page finalized

--- nodeJs/script.js
+++ nodeJs/script.js
@@ -7,3 +7,16 @@ for (const link of links) {
     break;
   }
 }
+
+const text = "Please fill out this form and I will get back to you as soon as possible.";
+let index = 0;
+
+function type() {
+  const typingText = document.getElementById("type-in");
+  typingText.innerHTML = text.slice(0, index++);
+  if (index <= text.length) {
+    setTimeout(type, 100);
+  }
+}
+
+type();
