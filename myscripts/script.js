56e6b2d3a5618c7ebc525e624302f6ff99b854fa
coletom22 <78000402+coletom22@users.noreply.github.com>
2023-04-16 19:00:46 -0700

    contact-hobbies-project COMPLETE, code structured according to format

---
+++ myscripts/script.js
@@ -0,0 +1,6 @@
+// Format the phone number input to (xxx)-xxx-xxxx
+const phoneInput = document.getElementById('phone');
+phoneInput.addEventListener('input', function (e) {
+  const phoneNumber = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !phoneNumber[2] ? phoneNumber[1] : `(${phoneNumber[1]})-${phoneNumber[2]}${phoneNumber[3] ? `-${phoneNumber[3]}` : ''}`;
+});
